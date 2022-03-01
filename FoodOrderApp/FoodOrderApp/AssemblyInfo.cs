39808936f8242f9a3843aed16148a5aa70e1979c
Alex VanLaningham <77026821+ajvanlaningham@users.noreply.github.com>
2022-03-01 16:15:50 -0500

    initial commit

---
+++ FoodOrderApp/FoodOrderApp/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)] 
+
+[assembly: ExportFont("CabinSketch-Bold.ttf", Alias = "CabinSketch")] 
+[assembly: ExportFont("materialdesigns-webfont.ttf", Alias ="FontAwesome")]
