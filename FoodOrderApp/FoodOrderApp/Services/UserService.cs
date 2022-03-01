86506d5160ece38238523f25d745dca0de3a2f87
Alex VanLaningham <77026821+ajvanlaningham@users.noreply.github.com>
2022-03-01 17:19:40 -0500

    pause for end of day. creatig the UserService.cs file that will handle user registration as well as the Firebase database url

---
+++ FoodOrderApp/FoodOrderApp/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Firebase.Database;
+using System.Threading.Tasks;
+using System.Linq;
+using FoodOrderApp.Models;
+
+namespace FoodOrderApp.Services
+{
+    public class UserService
+    {
+        FirebaseClient client;
+
+        public UserService()
+        {
+            client = new FirebaseClient("https://posv1-da974-default-rtdb.firebaseio.com/");
+        }
+
+        public async Task<bool> IsUserExist(string uname)
+        {
+            var user = (await client.Child("Users")
+                .OnceAsync<User>()).Where(u => u.Object.Username == uname).FirstOrDefault();
+
+            return (user != null);
+        }
+
+        public async Task<bool> RegisterUser(string uname, string passwd)
+        {
+            if  (await IsUserExist(uname) == false)
+            {
+                await client.Child("Users")
+                    .PostAsync(new User()
+                    {
+                        Username = uname,
+                        Password = passwd
+                    });
+            }
+        }
+    }
+}
